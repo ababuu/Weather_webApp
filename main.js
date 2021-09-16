65c5d57bf628340d34f54a51ac1b0b6a2e012d93
ababuu <ababuu@github.com>
2021-09-16 15:06:24 +0300

    icon and weather status added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+const body=document.querySelector('body');
 async function apiCall(){
     let response= await fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=cc6eab723af7048e058f86b0e00298e2',{mode: 'cors'});
     console.log(response.json());
@@ -20,6 +20,16 @@ async function getTempFeelsLike(){
     const feelsLike=data.main.feels_like;
     console.log(feelsLike);
 }
+async function getMaxTemp(){
+    let data= await getWeather('addis ababa');
+    const maxTemp=data.main.temp_max;
+    console.log(maxTemp);
+}
+async function getMinTemp(){
+    let data= await getWeather('addis ababa');
+    const minTemp=data.main.temp_min;
+    console.log(minTemp);
+}
 async function getHumidity(){
     let data= await getWeather('addis ababa');
     const humidity=data.main.humidity;
@@ -30,9 +40,24 @@ async function getPressure(){
     const pressure=data.main.pressure;
     console.log(pressure);
 }
+async function getTodayWeather(){
+    let data= await getWeather('addis ababa');
+    const weather=data.weather[0].main;
+    console.log(weather);
+}
+async function getIcon(){
+    let data= await getWeather('addis ababa');
+    const img=document.createElement('img');
+    img.src=`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    body.appendChild(img);
+}
 
 
 getTemperature();
 getTempFeelsLike();
+getMaxTemp();
+getMinTemp();
 getHumidity();
 getPressure();
+getTodayWeather();
+getIcon();
